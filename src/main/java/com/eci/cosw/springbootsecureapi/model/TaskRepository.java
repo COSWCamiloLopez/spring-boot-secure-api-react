73bee3ced0ecfcfcdd6f8699dfc742d609a1390f
Camilo Lopez <camilino123@gmail.com>
2019-03-20 17:41:41 -0500

    Find methods added to repositories

--- src/main/java/com/eci/cosw/springbootsecureapi/model/TaskRepository.java
+++ src/main/java/com/eci/cosw/springbootsecureapi/model/TaskRepository.java
@@ -2,5 +2,17 @@ package com.eci.cosw.springbootsecureapi.model;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface TaskRepository extends MongoRepository<Task, String> {
+
+    List<Task> findByOwner(Long owner);
+
+    List<Task> findByResponsible(Long responsible);
+
+    List<Task> findByDueDate(Date dueDate);
+
+    List<Task> findByStatus(String status);
+
 }
--- src/main/java/com/eci/cosw/springbootsecureapi/model/UserRepository.java
+++ src/main/java/com/eci/cosw/springbootsecureapi/model/UserRepository.java
@@ -2,5 +2,12 @@ package com.eci.cosw.springbootsecureapi.model;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String>{
+public interface UserRepository extends MongoRepository<User, String> {
+
+    User findById(Long id);
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
 }
